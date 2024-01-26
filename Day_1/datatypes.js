eac5ff4df17e0c08ebb2ca4e04a79fb035793a7e
Sofiyah123 <musasophiat@gmail.com>
2024-01-25 18:04:56 -0800

    Asabeneh Day 1 Exercises

---
+++ Day_1/datatypes.js
@@ -0,0 +1,9 @@
+// let myName = "Sofiyah";
+// let isMarried = true;
+// let emptyVariable = null;
+// let undefinedVariable;
+console.log(typeof myName);
+console.log(typeof isMarried);
+console.log(typeof emptyVariable);
+console.log(typeof undefinedVariable);
+
---
+++ Day_1/variable.js
@@ -0,0 +1,33 @@
+let myName = "Sofiyah";
+let isMarried = true;
+let emptyVariable = null;
+let undefinedVariable;
+console.log(myName, isMarried, emptyVariable, undefinedVariable);
+
+//Variables declaration
+let fullName;
+let $maritalStatus;
+let favoriteFood;
+let address;
+
+//variables declaration with assigned valued
+let surname = "Ibrahim";
+let DateOfBirth = "07/06/1997";
+let employmentStatus = "Unemployed";
+let sex = "female";
+
+// single line variable declaration
+let $firstName, $lastName, maritalStatus, $country, $age;
+
+// multiple lines variables declaration
+let firstName;
+let lastName;
+let HighestQualification;
+let country;
+let age;
+
+// Declaration of two variables with assigned values
+let myAge = 23;
+let yourAge = 30;
+console.log("I am " + myAge + "years old");
+console.log("You are " + yourAge + "years old");
